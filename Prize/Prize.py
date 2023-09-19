33ad00fb1ac0e1eda2cf0c87381c165885cc3897
Abraham Zheng <abrahamzheng@uhenry.com>
2023-09-19 19:39:11 +0800

    (feat): Add Prize HW in Python

---
+++ Prize/Prize.py
@@ -0,0 +1,11 @@
+a = input("Enter the number of prizes (1 for the first, 2 for the second, 3 fot the third, 0 for participate)")
+if (a == 1):
+    print("10000RMB! You will buy a new notebook")
+elif (a == 2):
+    print("5000RMB! You will buy a new iPad")
+elif (a == 3):
+    print("2000RMB! You will buy a new backpack")
+elif (a == 4):
+    print("500RMB! You will go out for a big meal")
+else:
+    print("Invalid input")
