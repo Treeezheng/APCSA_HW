2598e462ddeb1af0adab174118933cee0dc3e72f
Abraham Zheng <abrahamzheng@uhenry.com>
2023-09-20 07:45:42 +0800

    Delete

--- Prize/prize_rust_ver/target/debug/deps/prize_rust_ver-076744ffb5262454.d
+++
@@ -1,5 +0,0 @@
-/Users/treee/Code/APCSA_HW/Prize/prize_rust_ver/target/debug/deps/prize_rust_ver-076744ffb5262454.rmeta: src/main.rs
-
-/Users/treee/Code/APCSA_HW/Prize/prize_rust_ver/target/debug/deps/prize_rust_ver-076744ffb5262454.d: src/main.rs
-
-src/main.rs:
