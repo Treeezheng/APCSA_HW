11a1378a7bc9e409392a502343c1fca8438c81ca
Abraham Zheng <abrahamzheng@uhenry.com>
2023-09-20 07:35:33 +0800

    (feat): Add Prize HW in rust

---
+++ Prize/prize_rust_ver/target/debug/deps/prize_rust_ver-354cd365f5e01d81.d
@@ -0,0 +1,5 @@
+/Users/treee/Code/APCSA_HW/Prize/prize_rust_ver/target/debug/deps/prize_rust_ver-354cd365f5e01d81.rmeta: src/main.rs
+
+/Users/treee/Code/APCSA_HW/Prize/prize_rust_ver/target/debug/deps/prize_rust_ver-354cd365f5e01d81.d: src/main.rs
+
+src/main.rs:
