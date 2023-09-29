3519fd3ae529b69c9ca967c46b65cdc9821a3b82
Abraham Zheng <abrahamzheng@uhenry.com>
2023-09-29 22:44:43 +0800

    (fix): OutputSum

---
+++ OutputSum/Triple.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) {
+        // This code counts all the sum of even numbers from 1 to 100
+        int sum = 1, count = 0;
+        while (sum <= 100) {
+            if (sum % 3 == 0) {
+                count += sum;
+            }
+            sum++;
+        }
+        System.out.println(count);
+    }
+}
