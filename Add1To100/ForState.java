b1a8a47fa003f3935cc3160c09f5757065498465
Abraham Zheng <abrahamzheng@uhenry.com>
2023-10-08 11:00:35 +0800

    (feat): new code to add 1 to 100

---
+++ Add1To100/ForState.java
@@ -0,0 +1,9 @@
+public class ForState {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 1; i <= 100; i++) {
+            count += i;
+        }
+        System.out.println(count);
+    }
+}
