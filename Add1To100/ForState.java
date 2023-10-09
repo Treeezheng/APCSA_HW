e8efbc55639ed8c9f4ce37c797489d8624b094f6
Abraham Zheng <abrahamzheng@uhenry.com>
2023-10-09 21:27:16 +0800

    Change

--- Add1To100/ForState.java
+++ Add1To100/ForState.java
@@ -1,5 +1,13 @@
 public class ForState {
     public static void main(String[] args) {
+        try {
+            int[] array = {1, 2, 3};
+            for (int i = 0; i <= array.length; i++) {
+                System.out.println(array[i]);
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+
+    public static void main(String[] args) {
         int count = 0;
         for (int i = 1; i <= 100; i++) {
             count += i;
